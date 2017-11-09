5c6b2a70a5ffb3ab0fcbfe97b49e06f45d3c5918
Rishabh04-02 <rishabh0402@gmail.com>
2017-11-09 12:56:58 +0530

    modifications

--- get_ip.py
+++ get_ip.py
@@ -1,4 +1,4 @@
-with open('output','r') as f: #File opening in read mode
+with open('output','r') as f: #File opening in read mode, take input to output file as arp -e
     next(f) #Going to the next line (skipping the header row)
     for line in f: #Fetching content line by line
         print(line.split(' ')[0]) #Prints the first word of line
