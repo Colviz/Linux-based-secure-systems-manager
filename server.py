6b124f792b541ac41c41f3b67dacd92883342d82
rahul10mail <rahul10mail@gmail.com>
2017-11-09 13:38:21 +0530

    added get_my_ip

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 import socket
 
+def get_my_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
 def Main():
 	host = '192.168.43.130'
 	port = 5001
