235ac129828c4fcde9b0de81c24e950c47f13572
Rishabh Chaudhary <rishabh0402@gmail.com>
2017-12-12 02:36:32 +0530

    alignment

--- broadcasting_client.py
+++ broadcasting_client.py
@@ -6,7 +6,7 @@ import sys
 import select
 import subprocess
 
-def random_with_N_digits(n):			#Function for printing random no.'s
+def random_with_N_digits(n):		        	#Function for printing random no.'s
     range_start = 10**(n-1)
     range_end = (10**n)-1
     return randint(range_start, range_end)
@@ -19,16 +19,16 @@ def OSinfo(thisisit):					# OSinfo function returns 0 on success
     return osstdout
 
 host=""
-port = 9999								#Port being used by server
+port = 9999						#Port being used by server
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind(('',port))
 
 addr = (host,port)
-buf=1024								#Buffer size
+buf=1024					    	#Buffer size
 
 #file = random_with_N_digits(4)
 
-f = open("files/file.txt",'wb')			#Opening file in write mode
+f = open("files/file.txt",'wb')			        #Opening file in write mode
 data,addr = s.recvfrom(buf)
 try:
     while(data):
@@ -36,21 +36,21 @@ try:
         s.settimeout(2)
         data,addr = s.recvfrom(buf)
 except timeout:
-    f.close()							#Closing the opened file
-    s.close()							#Closing the socket connection
+    f.close()						#Closing the opened file
+    s.close()						#Closing the socket connection
 print("File Downloaded")
 
 
-filepath = "files/file.txt" 			#Path to file having commands. Every line has only one command 
+filepath = "files/file.txt" 			        #Path to file having commands. Every line has only one command 
 with open(filepath) as fp:  
    line = fp.readline()
    cnt = 1
    array = []
    while line:
-       print("Line {}: {}".format(cnt, line.strip())) # Print which line and command is executing
+       print("Line {}: {}".format(cnt, line.strip()))   #Print which line and command is executing
        j = OSinfo(line)   				#j is status returned by OSinfo
        array.append(j)
        line = fp.readline()
        cnt += 1
 
-print(array) 							#Prints all the statuses
+print(array) 						#Prints all the statuses
